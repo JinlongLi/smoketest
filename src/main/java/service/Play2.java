1c38c4ea3972fe2a3bdec15f450cb26db0f21c67
Jinlong Li <jinlong@stellar.aero>
2018-08-14 19:23:39 -0700

    add new files

--- src/main/java/service/Play2.java
+++ src/main/java/service/Play2.java
@@ -3,17 +3,23 @@ package service;
 import java.util.ArrayList;
 import java.util.List;
 
+import card.Card;
 import card.HoleCards;
 import game.Game;
 
-public class Play {
+public class Play2 {
   public static void main(String[] args) {
     List<HoleCards> hc = new ArrayList<>();
-    hc.add(new HoleCards("JsJh"));
-    hc.add(new HoleCards("KdQd"));
+    hc.add(new HoleCards("AsAh"));
+    hc.add(new HoleCards("JdQd"));
     int[] winner = new int[hc.size()];
+    List<Card> cc = new ArrayList<>();
+    cc.add(new Card("3d"));
+    cc.add(new Card("Td"));
+    cc.add(new Card("9s"));
+    cc.add(new Card("4d"));
     for (int i = 0; i < 1000; i++) {
-      Game game = new Game(hc);
+      Game game = new Game(hc, cc);
       game.dealHoleCards();
       game.dealCommunityCards();
       game.findWinners();
---
+++ src/test/java/utils/TestEnvironment.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class TestEnvironment {
+
+  public String getBaseUrlForService(Service service) {
+    return "http://localhost:8080/";
+  }
+
+  public String getBaseUrl() {
+    return "http://localhost:8080/";
+  }
+}
